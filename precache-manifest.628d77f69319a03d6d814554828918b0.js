bdaacec8a9a61a57ab9d4587f20a247c5acd7a70
DreamsOfImran <ibasha66@gmail.com>
2020-05-08 00:22:32 +0530

    Updates

---
+++ precache-manifest.628d77f69319a03d6d814554828918b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9e83c27ccb7c07ffd38a7fd177e125",
+    "url": "/dreamlibrary-frontend/index.html"
+  },
+  {
+    "revision": "0eb79a6614d45e9e70d9",
+    "url": "/dreamlibrary-frontend/static/css/2.b49456b9.chunk.css"
+  },
+  {
+    "revision": "d004f2899ac372f23037",
+    "url": "/dreamlibrary-frontend/static/css/main.d01c3ce6.chunk.css"
+  },
+  {
+    "revision": "0eb79a6614d45e9e70d9",
+    "url": "/dreamlibrary-frontend/static/js/2.c432f75a.chunk.js"
+  },
+  {
+    "revision": "5108608652127668af97ef6143747fc1",
+    "url": "/dreamlibrary-frontend/static/js/2.c432f75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d004f2899ac372f23037",
+    "url": "/dreamlibrary-frontend/static/js/main.6af3f0b6.chunk.js"
+  },
+  {
+    "revision": "e74d0cac7e2c328ef4a9",
+    "url": "/dreamlibrary-frontend/static/js/runtime-main.fc18a006.js"
+  }
+]);
